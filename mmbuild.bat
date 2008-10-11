1e97e471cb44235a5478430751eddc53f01bd2c8
nenad <nenad@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2008-10-11 06:22:44 +0000

    build batch modified but still not working...
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/branches/micromanager1.3@1636 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- mmbuild.bat
+++ mmbuild.bat
@@ -1,13 +1,13 @@
 ECHO building native libraries...
-cd "C:\Program Files\Microsoft Visual Studio 8\VC\"
+cd "C:\Program Files\Microsoft Visual Studio 9.0\VC\"
 call vcvarsall.bat
  
-cd "\projects\micromanager1.2\"
-devenv /REBUILD Release .\MMCoreJ_wrap\MMCoreJ_wrap_1.2.sln
+cd "\projects\micromanager1.3\"
+devenv /REBUILD Release .\MMCoreJ_wrap\MMCoreJ_wrap.sln
 
 ECHO Building Java components...
 cd mmStudio\src
-call \projects\3rdparty\apache-ant-1.6.5\bin\ant -buildfile ..\build_dist.xml cleanMMStudio compileMMStudio buildMMStudio buildMMReader install packInstaller
+call \projects\3rdparty\apache-ant-1.6.5\bin\ant -buildfile ..\build.xml cleanMMStudio compileMMStudio buildMMStudio buildMMReader install packInstaller
 
 ECHO "Done"
 cd "..\.."
