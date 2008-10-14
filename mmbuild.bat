35a8090852cda9d91133d8ef5328def1edecd371
nico <nico@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2008-10-14 21:22:27 +0000

    Added autofocus ant build to the mmbuild script
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/branches/micromanager1.3@1651 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- mmbuild.bat
+++ mmbuild.bat
@@ -6,6 +6,11 @@ cd "\projects\micromanager1.3\"
 devenv /REBUILD Release .\MMCoreJ_wrap\MMCoreJ_wrap.sln
 
 ECHO Building Java components...
+
+cd autofocus
+call \projects\3rdparty\apache-ant-1.6.5\bin\ant -buildfile build.xml cleanAutofocus compileAutofocus buildAutofocus 
+cd ..
+
 cd mmStudio\src
 call \projects\3rdparty\apache-ant-1.6.5\bin\ant -buildfile ..\build.xml cleanMMStudio compileMMStudio buildMMStudio buildMMReader install packInstaller
 
