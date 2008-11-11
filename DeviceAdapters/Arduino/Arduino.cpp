d51bc9b5366244ab5587aa3d78f9530f2238e834
nico <nico@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2008-11-11 03:20:56 +0000

    Adding adapter for Arduino digital IO device.  Needs special firmware (to be OPen Sourced)
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/branches/micromanager1.3@1724 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

---
+++ DeviceAdapters/Arduino/Arduino.cpp
@@ -0,0 +1,789 @@
+///////////////////////////////////////////////////////////////////////////////
+// FILE:          Arduino.cpp
+// PROJECT:       Micro-Manager
+// SUBSYSTEM:     DeviceAdapters
+//-----------------------------------------------------------------------------
+// DESCRIPTION:   Arduino adapter.  Needs acompanying firmware
+// COPYRIGHT:     University of California, San Francisco, 2008
+// LICENSE:       LGPL
+// 
+// AUTHOR:        Nico Stuurman, nico@cmp.ucsf.edu 11/09/2008
+//
+//
+
+#include "Arduino.h"
+#include "../../MMDevice/ModuleInterface.h"
+
+#ifdef WIN32
+   #define WIN32_LEAN_AND_MEAN
+   #include <windows.h>
+   #define snprintf _snprintf 
+#endif
+
+const char* g_DeviceNameArduinoHub = "Arduino-Hub";
+const char* g_DeviceNameArduinoSwitch = "Arduino-Switch";
+const char* g_DeviceNameArduinoShutter = "Arduino-Shutter";
+const char* g_DeviceNameArduinoDA = "Arduino-DAC";
+const char* g_DeviceNameArduinoInput = "Arduino-Input";
+
+std::string g_port;
+
+// Global state of the Arduino switch to enable simulation of the shutter device.
+// The virtual shutter device uses this global variable to restore state of the switch
+unsigned g_switchState_ = 0;
+unsigned g_shutterState_ = 0;
+bool g_boardInitialized_ = false;
+bool g_invertedLogic = false;
+const char* g_normalLogicString = "Normal";
+const char* g_invertedLogicString = "Inverted";
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Exported MMDevice API
+///////////////////////////////////////////////////////////////////////////////
+MODULE_API void InitializeModuleData()
+{
+   AddAvailableDeviceName(g_DeviceNameArduinoHub, "Hub");
+   AddAvailableDeviceName(g_DeviceNameArduinoSwitch, "Switch");
+   AddAvailableDeviceName(g_DeviceNameArduinoShutter, "Shutter");
+   AddAvailableDeviceName(g_DeviceNameArduinoDA, "DA");
+   AddAvailableDeviceName(g_DeviceNameArduinoInput, "Input");
+}
+
+MODULE_API MM::Device* CreateDevice(const char* deviceName)
+{
+   if (deviceName == 0)
+      return 0;
+
+   if (strcmp(deviceName, g_DeviceNameArduinoHub) == 0)
+   {
+      return new CArduinoHub;
+   }
+   else if (strcmp(deviceName, g_DeviceNameArduinoSwitch) == 0)
+   {
+      return new CArduinoSwitch;
+   }
+   else if (strcmp(deviceName, g_DeviceNameArduinoShutter) == 0)
+   {
+      return new CArduinoShutter;
+   }
+   /*
+   else if (strcmp(deviceName, g_DeviceNameArduinoDA) == 0)
+   {
+      return new CArduinoDA();
+   }
+   else if (strcmp(deviceName, g_DeviceNameArduinoInput) == 0)
+   {
+      return new CArduinoInput;
+   }
+   */
+
+
+   return 0;
+}
+
+MODULE_API void DeleteDevice(MM::Device* pDevice)
+{
+   delete pDevice;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// CArduinoHUb implementation
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+CArduinoHub::CArduinoHub() :
+initialized_ (false)
+{
+   InitializeDefaultErrorMessages();
+
+   SetErrorText(ERR_PORT_OPEN_FAILED, "Failed opening Velleman Arduino USB device");
+
+   CPropertyAction* pAct = new CPropertyAction(this, &CArduinoHub::OnPort);
+   CreateProperty(MM::g_Keyword_Port, "Undefined", MM::String, false, pAct, true);
+
+   pAct = new CPropertyAction(this, &CArduinoHub::OnLogic);
+   CreateProperty("Logic", g_invertedLogicString, MM::String, false, pAct, true);
+
+   AddAllowedValue("Logic", g_invertedLogicString);
+   AddAllowedValue("Logic", g_normalLogicString);
+}
+
+CArduinoHub::~CArduinoHub()
+{
+   Shutdown();
+}
+
+void CArduinoHub::GetName(char* name) const
+{
+   CDeviceUtils::CopyLimitedString(name, g_DeviceNameArduinoHub);
+}
+
+bool CArduinoHub::Busy()
+{
+   return false;
+}
+
+int CArduinoHub::Initialize()
+{
+   // Name
+   int ret = CreateProperty(MM::g_Keyword_Name, g_DeviceNameArduinoHub, MM::String, true);
+   if (DEVICE_OK != ret)
+      return ret;
+
+   ret = UpdateStatus();
+   if (ret != DEVICE_OK)
+      return ret;
+
+   initialized_ = true;
+   return DEVICE_OK;
+}
+
+int CArduinoHub::Shutdown()
+{
+   initialized_ = false;
+   return DEVICE_OK;
+}
+
+int CArduinoHub::OnPort(MM::PropertyBase* pProp, MM::ActionType pAct)
+{
+   if (pAct == MM::BeforeGet)
+   {
+      pProp->Set(g_port.c_str());
+   }
+   else if (pAct == MM::AfterSet)
+   {
+      pProp->Get(g_port);
+      /*
+      g_ArduinoInterface.SetPort(port_);
+      g_ArduinoInterface.initialized_ = true;
+      int ret = g_ArduinoInterface.OpenDevice();
+      if (ret != DEVICE_OK)
+         return ret;
+      */
+   }
+   return DEVICE_OK;
+}
+
+int CArduinoHub::OnLogic(MM::PropertyBase* pProp, MM::ActionType pAct)
+{
+   if (pAct == MM::BeforeGet)
+   {
+      if (g_invertedLogic)
+         pProp->Set(g_invertedLogicString);
+      else
+         pProp->Set(g_normalLogicString);
+   } else if (pAct == MM::AfterSet)
+   {
+      std::string logic;
+      pProp->Get(logic);
+      if (logic.compare(g_invertedLogicString)==0)
+         g_invertedLogic = true;
+      else g_invertedLogic = false;
+   }
+   return DEVICE_OK;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// CArduinoSwitch implementation
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+CArduinoSwitch::CArduinoSwitch() : numPos_(64), busy_(false)
+{
+   InitializeDefaultErrorMessages();
+
+   // add custom error messages
+   SetErrorText(ERR_UNKNOWN_POSITION, "Invalid position (state) specified");
+   SetErrorText(ERR_INITIALIZE_FAILED, "Initialization of the device failed");
+   SetErrorText(ERR_WRITE_FAILED, "Failed to write data to the device");
+   SetErrorText(ERR_CLOSE_FAILED, "Failed closing the device");
+}
+
+CArduinoSwitch::~CArduinoSwitch()
+{
+   Shutdown();
+}
+
+void CArduinoSwitch::GetName(char* name) const
+{
+   CDeviceUtils::CopyLimitedString(name, g_DeviceNameArduinoSwitch);
+}
+
+
+int CArduinoSwitch::Initialize()
+{
+   if (!g_boardInitialized_) {
+      // int ret = InitializeTheBoard();
+      // if (ret != DEVICE_OK)
+      //  return ret;
+   }
+
+   // set property list
+   // -----------------
+   
+   // Name
+   int nRet = CreateProperty(MM::g_Keyword_Name, g_DeviceNameArduinoSwitch, MM::String, true);
+   if (DEVICE_OK != nRet)
+      return nRet;
+
+   // Description
+   nRet = CreateProperty(MM::g_Keyword_Description, "Arduino digital output driver", MM::String, true);
+   if (DEVICE_OK != nRet)
+      return nRet;
+
+   // create positions and labels
+   const int bufSize = 65;
+   char buf[bufSize];
+   for (long i=0; i<numPos_; i++)
+   {
+      snprintf(buf, bufSize, "%d", (unsigned)i);
+      SetPositionLabel(i, buf);
+   }
+
+   // State
+   // -----
+   CPropertyAction* pAct = new CPropertyAction (this, &CArduinoSwitch::OnState);
+   nRet = CreateProperty(MM::g_Keyword_State, "0", MM::Integer, false, pAct);
+   if (nRet != DEVICE_OK)
+      return nRet;
+   SetPropertyLimits(MM::g_Keyword_State, 0, numPos_ - 1);
+
+   // Label
+   // -----
+   pAct = new CPropertyAction (this, &CStateBase::OnLabel);
+   nRet = CreateProperty(MM::g_Keyword_Label, "", MM::String, false, pAct);
+   if (nRet != DEVICE_OK)
+      return nRet;
+
+   nRet = UpdateStatus();
+   if (nRet != DEVICE_OK)
+      return nRet;
+
+   initialized_ = true;
+
+   return DEVICE_OK;
+}
+
+int CArduinoSwitch::Shutdown()
+{
+   initialized_ = false;
+   return DEVICE_OK;
+}
+
+int CArduinoSwitch::WriteToPort(long value)
+{
+   value = 63 & value;
+   if (g_invertedLogic)
+      value = ~value;
+
+   std::string command;
+   command[0] = 'S';
+   command[1] = (char) value;
+   int ret = WriteToComPort(g_port.c_str(), (const unsigned char*) command.c_str(), 2);
+   if (ret != DEVICE_OK)
+      return ret;
+
+   std::string answer;
+   ret = GetSerialAnswer(g_port.c_str(), ":", answer);
+   if (ret != DEVICE_OK)
+      return ret;
+
+   return DEVICE_OK;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Action handlers
+///////////////////////////////////////////////////////////////////////////////
+
+int CArduinoSwitch::OnState(MM::PropertyBase* pProp, MM::ActionType eAct)
+{
+   if (eAct == MM::BeforeGet)
+   {
+      // nothing to do, let the caller to use cached property
+   }
+   else if (eAct == MM::AfterSet)
+   {
+      long pos;
+      pProp->Get(pos);
+      g_switchState_ = pos;
+      if (g_shutterState_ > 0)
+         return WriteToPort(pos);
+   }
+
+   return DEVICE_OK;
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+// CArduinoDA implementation
+// ~~~~~~~~~~~~~~~~~~~~~~
+
+CArduinoDA::CArduinoDA() :
+      busy_(false), 
+      minV_(0.0), 
+      maxV_(0.0), 
+      volts_(0.0),
+      gatedVolts_(0.0),
+      encoding_(0), 
+      resolution_(8), 
+      channel_(1), 
+      name_(""),
+      gateOpen_(true)
+{
+   InitializeDefaultErrorMessages();
+
+   // add custom error messages
+   SetErrorText(ERR_UNKNOWN_POSITION, "Invalid position (state) specified");
+   SetErrorText(ERR_INITIALIZE_FAILED, "Initialization of the device failed");
+   SetErrorText(ERR_WRITE_FAILED, "Failed to write data to the device");
+   SetErrorText(ERR_CLOSE_FAILED, "Failed closing the device");
+
+   CPropertyAction* pAct = new CPropertyAction(this, &CArduinoDA::OnChannel);
+   CreateProperty("Channel", "1", MM::Integer, false, pAct, true);
+   for (int i=1; i< 9; i++){
+      std::ostringstream os;
+      os << i;
+      AddAllowedValue("Channel", os.str().c_str());
+   }
+
+   pAct = new CPropertyAction(this, &CArduinoDA::OnMaxVolt);
+   CreateProperty("MaxVolt", "5.0", MM::Float, false, pAct, true);
+   AddAllowedValue("MaxVolt", "5.0");
+   AddAllowedValue("MaxVolt", "10.0");
+}
+
+CArduinoDA::~CArduinoDA()
+{
+   Shutdown();
+}
+
+void CArduinoDA::GetName(char* name) const
+{
+   CDeviceUtils::CopyLimitedString(name, name_.c_str());
+}
+
+
+int CArduinoDA::Initialize()
+{
+   // obtain scaling info
+
+   minV_ = 0.0;
+   maxV_ = 5.0;
+
+   // set property list
+   // -----------------
+   
+   // Name
+   int nRet = CreateProperty(MM::g_Keyword_Name, name_.c_str(), MM::String, true);
+   if (DEVICE_OK != nRet)
+      return nRet;
+
+   // Description
+   nRet = CreateProperty(MM::g_Keyword_Description, "Arduino DAC driver", MM::String, true);
+   if (DEVICE_OK != nRet)
+      return nRet;
+
+   // State
+   // -----
+   CPropertyAction* pAct = new CPropertyAction (this, &CArduinoDA::OnVolts);
+   nRet = CreateProperty("Volts", "0.0", MM::Float, false, pAct);
+   if (nRet != DEVICE_OK)
+      return nRet;
+   SetPropertyLimits("Volts", 0.0, 5.0);
+
+   nRet = UpdateStatus();
+   if (nRet != DEVICE_OK)
+      return nRet;
+
+   initialized_ = true;
+
+   return DEVICE_OK;
+}
+
+int CArduinoDA::Shutdown()
+{
+   initialized_ = false;
+   return DEVICE_OK;
+}
+
+int CArduinoDA::WriteToPort(long value)
+{
+   /*
+   int ret = g_ArduinoInterface.OutputAnalogChannel(*this, *GetCoreCallback(), channel_, (int) value);
+   if (ret != DEVICE_OK)
+   {
+      this->LogMessage("Error reported by libk8055 function WriteAllDigital");
+      return ret;
+   }
+   */
+
+   return DEVICE_OK;
+}
+
+int CArduinoDA::WriteSignal(double volts)
+{
+   /*
+   long value = (long) ((1L<<resolution_)/((float)maxV_ - (float)minV_) * (volts - (float)minV_));
+   value = min((1L<<resolution_)-1,value);
+
+   if (encoding_ != 0) {
+      // convert to 2's comp by inverting the sign bit
+      long sign = 1L << (resolution_ - 1);
+      value ^= sign;
+      if (value & sign)           //sign extend
+         value |= 0xffffffffL << resolution_;
+   }
+   return WriteToPort(value);
+   */
+   return DEVICE_OK;
+}
+
+int CArduinoDA::SetSignal(double volts)
+{
+   volts_ = volts;
+   if (gateOpen_) {
+      gatedVolts_ = volts_;
+      return WriteSignal(volts_);
+   } else {
+      gatedVolts_ = 0;
+   }
+
+   return DEVICE_OK;
+}
+
+int CArduinoDA::SetGateOpen(bool open)
+{
+   if (open) {
+      gateOpen_ = true;
+      gatedVolts_ = volts_;
+      return WriteSignal(volts_);
+   } else {
+      gateOpen_ = false;
+      gatedVolts_ = 0;
+      return WriteSignal(0.0);
+   }
+
+   return DEVICE_OK;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Action handlers
+///////////////////////////////////////////////////////////////////////////////
+
+int CArduinoDA::OnVolts(MM::PropertyBase* pProp, MM::ActionType eAct)
+{
+   if (eAct == MM::BeforeGet)
+   {
+      // nothing to do, let the caller use cached property
+   }
+   else if (eAct == MM::AfterSet)
+   {
+      double volts;
+      pProp->Get(volts);
+      return SetSignal(volts);
+   }
+
+   return DEVICE_OK;
+}
+
+int CArduinoDA::OnMaxVolt(MM::PropertyBase* pProp, MM::ActionType eAct)
+{
+   if (eAct == MM::BeforeGet)
+   {
+      pProp->Set(maxV_);
+   }
+   else if (eAct == MM::AfterSet)
+   {
+      double maxV;
+      pProp->Get(maxV);
+      if (maxV == 5.0 || maxV == 10.0)
+         maxV_ = maxV;
+      if (maxV == 5.0)
+         SetPropertyLimits("Volts", 0.0, 5.0);
+      else if (maxV == 10.0)
+         SetPropertyLimits("Volts", 0.0, 10.0);
+
+   }
+   return DEVICE_OK;
+}
+
+int CArduinoDA::OnChannel(MM::PropertyBase* pProp, MM::ActionType eAct)
+{
+   if (eAct == MM::BeforeGet)
+   {
+      pProp->Set((long int)channel_);
+   }
+   else if (eAct == MM::AfterSet)
+   {
+      long channel;
+      pProp->Get(channel);
+      if (channel >=1 && channel <=8)
+         channel_ = channel;
+   }
+   return DEVICE_OK;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// CArduinoShutter implementation
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+CArduinoShutter::CArduinoShutter() : initialized_(false), name_(g_DeviceNameArduinoShutter)
+{
+   InitializeDefaultErrorMessages();
+   EnableDelay();
+}
+
+CArduinoShutter::~CArduinoShutter()
+{
+   Shutdown();
+}
+
+void CArduinoShutter::GetName(char* name) const
+{
+   assert(name_.length() < CDeviceUtils::GetMaxStringLength());
+   CDeviceUtils::CopyLimitedString(name, name_.c_str());
+}
+
+bool CArduinoShutter::Busy()
+{
+   MM::MMTime interval = GetCurrentMMTime() - changedTime_;
+
+   if (interval < (1000.0 < GetDelayMs() ))
+      return true;
+   else
+       return false;
+}
+
+int CArduinoShutter::Initialize()
+{
+   // set property list
+   // -----------------
+   
+   // Name
+   int ret = CreateProperty(MM::g_Keyword_Name, name_.c_str(), MM::String, true);
+   if (DEVICE_OK != ret)
+      return ret;
+
+   // Description
+   ret = CreateProperty(MM::g_Keyword_Description, "Arduino shutter driver", MM::String, true);
+   if (DEVICE_OK != ret)
+      return ret;
+
+   // OnOff
+   // ------
+   CPropertyAction* pAct = new CPropertyAction (this, &CArduinoShutter::OnOnOff);
+   ret = CreateProperty("OnOff", "0", MM::Integer, false, pAct);
+   if (ret != DEVICE_OK)
+      return ret;
+
+   // set shutter into the off state
+   //WriteToPort(0);
+
+   std::vector<std::string> vals;
+   vals.push_back("0");
+   vals.push_back("1");
+   ret = SetAllowedValues("OnOff", vals);
+   if (ret != DEVICE_OK)
+      return ret;
+
+   ret = UpdateStatus();
+   if (ret != DEVICE_OK)
+      return ret;
+
+   changedTime_ = GetCurrentMMTime();
+   initialized_ = true;
+
+   return DEVICE_OK;
+}
+
+int CArduinoShutter::Shutdown()
+{
+   if (initialized_)
+   {
+      initialized_ = false;
+   }
+   return DEVICE_OK;
+}
+
+int CArduinoShutter::SetOpen(bool open)
+{
+   if (open)
+      return SetProperty("OnOff", "1");
+   else
+      return SetProperty("OnOff", "0");
+}
+
+int CArduinoShutter::GetOpen(bool& open)
+{
+   char buf[MM::MaxStrLength];
+   int ret = GetProperty("OnOff", buf);
+   if (ret != DEVICE_OK)
+      return ret;
+   long pos = atol(buf);
+   pos > 0 ? open = true : open = false;
+
+   return DEVICE_OK;
+}
+
+int CArduinoShutter::Fire(double /*deltaT*/)
+{
+   return DEVICE_UNSUPPORTED_COMMAND;
+}
+
+int CArduinoShutter::WriteToPort(long value)
+{
+   if (g_invertedLogic)
+      value = (63 & ~value);
+
+   std::string command;
+   command[0] = 'S';
+   command[1] = (char) value;
+   int ret = WriteToComPort(g_port.c_str(), (const unsigned char*) command.c_str(), 2);
+   if (ret != DEVICE_OK)
+      return ret;
+
+   std::string answer;
+   ret = GetSerialAnswer(g_port.c_str(), ":", answer);
+   if (ret != DEVICE_OK)
+      return ret;
+
+   return DEVICE_OK;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Action handlers
+///////////////////////////////////////////////////////////////////////////////
+
+int CArduinoShutter::OnOnOff(MM::PropertyBase* pProp, MM::ActionType eAct)
+{
+   if (eAct == MM::BeforeGet)
+   {
+      // use cached state
+      pProp->Set((long)g_shutterState_);
+   }
+   else if (eAct == MM::AfterSet)
+   {
+      long pos;
+      pProp->Get(pos);
+      int ret;
+      if (pos == 0)
+         ret = WriteToPort(0); // turn everything off
+      else
+         ret = WriteToPort(g_switchState_); // restore old setting
+      if (ret != DEVICE_OK)
+         return ret;
+      g_shutterState_ = pos;
+      changedTime_ = GetCurrentMMTime();
+   }
+
+   return DEVICE_OK;
+}
+
+
+/*
+ * Reads digital and analogue input from Velleman board
+ * Mainly to test read functions in USBManager
+ */
+
+CArduinoInput::CArduinoInput() :
+name_(g_DeviceNameArduinoInput)
+{
+}
+
+CArduinoInput::~CArduinoInput()
+{
+   Shutdown();
+}
+
+int CArduinoInput::Shutdown()
+{
+   initialized_ = false;
+   return DEVICE_OK;
+}
+
+int CArduinoInput::Initialize()
+{
+   // Name
+   int ret = CreateProperty(MM::g_Keyword_Name, name_.c_str(), MM::String, true);
+   if (DEVICE_OK != ret)
+      return ret;
+
+   // Description
+   ret = CreateProperty(MM::g_Keyword_Description, "Arduino input", MM::String, true);
+   if (DEVICE_OK != ret)
+      return ret;
+
+   // Digital Input
+   CPropertyAction* pAct = new CPropertyAction (this, &CArduinoInput::OnDigitalInput);
+   ret = CreateProperty("Digital Input", "0", MM::Integer, true, pAct);
+   if (ret != DEVICE_OK)
+      return ret;
+
+   for (long i=1; i <=8; i++) 
+   {
+      CPropertyActionEx *pExAct = new CPropertyActionEx(this, &CArduinoInput::OnAnalogInput, i);
+      std::ostringstream os;
+      os << "Analogue Input A" << i;
+      ret = CreateProperty(os.str().c_str(), "0.0", MM::Float, true, pExAct);
+      if (ret != DEVICE_OK)
+         return ret;
+   }
+
+   ret = UpdateStatus();
+   if (ret != DEVICE_OK)
+      return ret;
+
+   initialized_ = true;
+
+   return DEVICE_OK;
+}
+
+void CArduinoInput::GetName(char* name) const
+{
+  CDeviceUtils::CopyLimitedString(name, name_.c_str());
+}
+
+bool CArduinoInput::Busy()
+{
+   return false;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Action handlers
+///////////////////////////////////////////////////////////////////////////////
+
+int CArduinoInput::OnDigitalInput(MM::PropertyBase* pProp, MM::ActionType eAct)
+{
+   if (eAct == MM::BeforeGet)
+   {
+      /*
+      unsigned char result;
+      int ret =  g_ArduinoInterface.ReadAllDigital(*this, *GetCoreCallback(), result);
+      if (ret != DEVICE_OK)
+         return ret;
+      pProp->Set((long)result);
+      */
+   }
+
+   return DEVICE_OK;
+}
+
+int CArduinoInput::OnAnalogInput(MM::PropertyBase* pProp, MM::ActionType eAct, long channel)
+{
+   if (eAct == MM::BeforeGet)
+   {
+      /*
+      long result;
+      int ret = g_ArduinoInterface.ReadAnalogChannel(*this, *GetCoreCallback(), (unsigned char) channel, result);
+      if (ret != DEVICE_OK)
+         return ret;
+
+      pProp->Set(result);
+      */
+   }
+   return DEVICE_OK;
+}
+
