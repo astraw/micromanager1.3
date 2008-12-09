d5f0a1cf8eb70063bb3fca80c8192ad08e424ce2
OD <OD@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2008-12-09 00:57:11 +0000

    Changed operation with XYZ control to support Normal, Ctrl, Shift and Alt
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/branches/micromanager1.3@1835 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- mmstudio/src/org/micromanager/navigation/XYZKeyListener.java
+++ mmstudio/src/org/micromanager/navigation/XYZKeyListener.java
@@ -29,211 +29,242 @@ import mmcorej.MMCoreJ;
 import ij.gui.*;
 import mmcorej.CMMCore;
 import org.micromanager.utils.MMLogger;
+
 /**
  * @author OD
  *
  */
 public final class XYZKeyListener implements KeyListener {
-	   private CMMCore core_;
-	   private ImageCanvas canvas_;
-	   private static boolean isRunning_ = false;
-	   private boolean mirrorX_;
-	   private boolean mirrorY_;
-	   private boolean transposeXY_;
-	   private boolean correction_;
-	   private static final double zmoveIncrement_ = 0.20;
-	   private static final double xymoveIncrement_ = 1.0;
-	   public static int slowStep=1;
-	   public static int normalStep=2;
-	   public static int fastStep=10;
-	   private int step;
-	  
-	   
-	   public XYZKeyListener(CMMCore core) {
-		      core_ = core;
-		   }	   
-	   
+	private CMMCore core_;
+	private ImageCanvas canvas_;
+	private static boolean isRunning_ = false;
+	private boolean mirrorX_;
+	private boolean mirrorY_;
+	private boolean transposeXY_;
+	private boolean correction_;
+	private static final double zmoveIncrement_ = 0.20;
+	public static int ctrlZStep = 1;
+	public static int normaZlStep = 5;
+	public static int shiftZStep = 10;
+	public static int ctrlXYStep = 1;
+	public static int normalXYStep = 10;
+	private int step;
+	private double stepX;
+	private double stepY;
+
+	public XYZKeyListener(CMMCore core) {
+		core_ = core;
+	}
+
 	public void keyPressed(KeyEvent e) {
 
-		MMLogger.getLogger().info(String.valueOf(e.getKeyCode()));
-		
-		step=slowStep;   
-		if(e.isControlDown())
-			   step=normalStep;
-		   else if (e.isShiftDown())
-			   step=fastStep;
-		
-		switch (e.getKeyCode())
-		{
+		MMLogger.getLogger().info(Integer.toString(e.getKeyCode()));
+
+		switch (e.getKeyCode()) {
 		case KeyEvent.VK_LEFT:
-		{
-			IncrementXY(-step,0);
-		}break;
 		case KeyEvent.VK_RIGHT:
-		{
-			IncrementXY(step,0);
-		}break;
 		case KeyEvent.VK_UP:
-		{
-			IncrementXY(0,step);
-		}break;
 		case KeyEvent.VK_DOWN:
-		{
-			IncrementXY(0,-step);
-		}break;
+			//XY step
+			stepX = stepY = normalXYStep;
+			if (e.isControlDown()) {
+				stepX = stepY = ctrlXYStep;
+			} else if (e.isShiftDown()) {
+				stepX = core_.getImageWidth();
+				stepY = core_.getImageHeight();
+			} else if (e.isAltDown()) {
+				stepX = core_.getImageWidth() / 2;
+				stepY = core_.getImageHeight() / 2;
+			}
+			switch (e.getKeyCode()) {
+			case KeyEvent.VK_LEFT:
+				IncrementXY(-stepX, 0);
+				break;
+			case KeyEvent.VK_RIGHT:
+				IncrementXY(stepX, 0);
+				break;
+			case KeyEvent.VK_UP:
+				IncrementXY(0, stepY);
+				break;
+			case KeyEvent.VK_DOWN:
+				IncrementXY(0, -stepY);
+			}
+			break;
 		case KeyEvent.VK_LESS:
 		case KeyEvent.VK_COMMA:
-		{
-			IncrementZ(-step);
-		}break;
 		case KeyEvent.VK_GREATER:
 		case KeyEvent.VK_PERIOD:
-		{
-			IncrementZ(step);
-		}break;
-		default:
+			step = ctrlZStep;
+			if (e.isControlDown())
+				step = normaZlStep;
+			else if (e.isShiftDown())
+				step = shiftZStep;
+			switch (e.getKeyCode()) {
+			case KeyEvent.VK_LESS:
+			case KeyEvent.VK_COMMA:
+				IncrementZ(-step);
+				break;
+			case KeyEvent.VK_GREATER:
+			case KeyEvent.VK_PERIOD:
+				IncrementZ(step);
+			}
 		}
-		
 	}
-	public void keyReleased(KeyEvent arg0) {}
-	public void keyTyped(KeyEvent arg0) {}
-	
-    public void start () {
-	   // Get a handle to the AcqWindow
-	   if (WindowManager.getCurrentWindow() != null) {
-	      start (WindowManager.getCurrentWindow());
-	   }
+
+	public void keyReleased(KeyEvent arg0) {
 	}
-	
-   public void start (ImageWindow img) {
-	  if (isRunning_)
-	     stop(); 
-	  
-      isRunning_ = true;
-	  if (img != null) {
-		  attach(img);
-	  }
-	  getOrientation();
-   }
 
-      
-	   public void stop() {
-	      if (canvas_ != null) {
-	         canvas_.removeKeyListener(this);
-	      }
-	      isRunning_ = false;
-	   }
+	public void keyTyped(KeyEvent arg0) {
+	}
 
-	   public boolean isRunning() {
-	      return isRunning_;
-	   }
+	public void start() {
+		// Get a handle to the AcqWindow
+		if (WindowManager.getCurrentWindow() != null) {
+			start(WindowManager.getCurrentWindow());
+		}
+	}
 
-	   public void attach(ImageWindow win) {
-	      if (!isRunning_)
-	         return;
-	      canvas_ = win.getCanvas();
-	      canvas_.addKeyListener(this);
-	   }
+	public void start(ImageWindow img) {
+		if (isRunning_)
+			stop();
 
-	   public void IncrementXY(int stepX, int stepY) {
-		      // Get needed info from core
-		      getOrientation();
-		      String xyStage = core_.getXYStageDevice();
-		      if (xyStage == null)
-		         return;
-		      try {
-		         if (core_.deviceBusy(xyStage))
-		            return;
-		      } catch (Exception ex) {
-		         JOptionPane.showMessageDialog(null, ex.getMessage()); 
-		         return;
-		      }
+		isRunning_ = true;
+		if (img != null) {
+			attach(img);
+		}
+		getOrientation();
+	}
 
-		      double pixSizeUm = core_.getPixelSizeUm();
-		      if (! (pixSizeUm > 0.0)) {
-		         JOptionPane.showMessageDialog(null, "Please provide pixel size calibration data before using this function");
-		         return;
-		      }
+	public void stop() {
+		if (canvas_ != null) {
+			canvas_.removeKeyListener(this);
+		}
+		isRunning_ = false;
+	}
 
-		      // calculate needed relative movement
-		      double tmpXUm = xymoveIncrement_*stepX;
-		      double tmpYUm = xymoveIncrement_*stepY;
+	public boolean isRunning() {
+		return isRunning_;
+	}
 
-		      tmpXUm *= pixSizeUm;
-		      tmpYUm *= pixSizeUm;
-		      double mXUm = tmpXUm;
-		      double mYUm = tmpYUm;
-		      // if camera does not correct image orientation, we'll correct for it here:
-		      if (!correction_) {
-		         // Order: swapxy, then mirror axis
-		         if (transposeXY_) {mXUm = tmpYUm; mYUm = tmpXUm;}
-		         if (mirrorX_) {mXUm = -mXUm;}
-		         if (mirrorY_) {mYUm = -mYUm;}
-		      }
+	public void attach(ImageWindow win) {
+		if (!isRunning_)
+			return;
+		canvas_ = win.getCanvas();
+		canvas_.addKeyListener(this);
+	}
 
-		      // Move the stage
-		      try {
-		         core_.setRelativeXYPosition(xyStage, mXUm, mYUm);
-		      } catch (Exception ex) {
-		         JOptionPane.showMessageDialog(null, ex.getMessage()); 
-		         return;
-		      }
-		   } 
-	   
-	   
-	   public void IncrementZ(int step) {
-	      // Get needed info from core
-	      String zStage = core_.getFocusDevice();
-	      if (zStage == null)
-	         return;
-	 
-	      double moveIncrement = zmoveIncrement_*step;
-	      double pixSizeUm = core_.getPixelSizeUm();
-	      if (pixSizeUm > 0.0) {
-	         moveIncrement = 2 * pixSizeUm;
-	      }
+	public void IncrementXY(double stepX, double stepY) {
+		// Get needed info from core
+		getOrientation();
+		String xyStage = core_.getXYStageDevice();
+		if (xyStage == null)
+			return;
+		try {
+			if (core_.deviceBusy(xyStage))
+				return;
+		} catch (Exception ex) {
+			JOptionPane.showMessageDialog(null, ex.getMessage());
+			return;
+		}
 
-	      // Move the stage
-	      try {
-	         core_.setRelativePosition(zStage, moveIncrement);
-	      } catch (Exception ex) {
-	         JOptionPane.showMessageDialog(null, ex.getMessage()); 
-	         return;
-	      }
+		double pixSizeUm = core_.getPixelSizeUm();
+		if (!(pixSizeUm > 0.0)) {
+			JOptionPane
+					.showMessageDialog(null,
+							"Please provide pixel size calibration data before using this function");
+			return;
+		}
 
-	   } 
-	   public void getOrientation() {
-		      String camera = core_.getCameraDevice();
-		      if (camera == null) {
-		         JOptionPane.showMessageDialog(null, "This function does not work without a camera");
-		         return;
-		      }
-		      try {
-		         String tmp = core_.getProperty(camera, "TransposeCorrection");
-		         if (tmp.equals("0")) 
-		            correction_ = false;
-		         else 
-		            correction_ = true;
-		         tmp = core_.getProperty(camera, MMCoreJ.getG_Keyword_Transpose_MirrorX());
-		         if (tmp.equals("0")) 
-		            mirrorX_ = false;
-		         else 
-		            mirrorX_ = true;
-		         tmp = core_.getProperty(camera, MMCoreJ.getG_Keyword_Transpose_MirrorY());
-		         if (tmp.equals("0")) 
-		            mirrorY_ = false;
-		         else 
-		            mirrorY_ = true;
-		         tmp = core_.getProperty(camera, MMCoreJ.getG_Keyword_Transpose_SwapXY());
-		         if (tmp.equals("0")) 
-		            transposeXY_ = false;
-		         else 
-		            transposeXY_ = true;
-		      } catch(Exception exc) {
-		         exc.printStackTrace();
-		         JOptionPane.showMessageDialog(null, "Exception encountered. Please report to the Micro-Manager support team");
-		         return;
-		      }
-		   }
+		// calculate needed relative movement
+		double tmpXUm = stepX * pixSizeUm;
+		double tmpYUm = stepY * pixSizeUm;
+
+		double mXUm = tmpXUm;
+		double mYUm = tmpYUm;
+		// if camera does not correct image orientation, we'll correct for it here:
+		if (!correction_) {
+			// Order: swapxy, then mirror axis
+			if (transposeXY_) {
+				mXUm = tmpYUm;
+				mYUm = tmpXUm;
+			}
+			if (mirrorX_) {
+				mXUm = -mXUm;
+			}
+			if (mirrorY_) {
+				mYUm = -mYUm;
+			}
+		}
+
+		// Move the stage
+		try {
+			core_.setRelativeXYPosition(xyStage, mXUm, mYUm);
+		} catch (Exception ex) {
+			JOptionPane.showMessageDialog(null, ex.getMessage());
+			return;
+		}
+	}
+
+	public void IncrementZ(int step) {
+		// Get needed info from core
+		String zStage = core_.getFocusDevice();
+		if (zStage == null)
+			return;
+
+		double moveIncrement = zmoveIncrement_;
+		double pixSizeUm = core_.getPixelSizeUm();
+		if (pixSizeUm > 0.0) {
+			moveIncrement = 2 * pixSizeUm;
+		}
+
+		// Move the stage
+		try {
+			core_.setRelativePosition(zStage, moveIncrement * step);
+		} catch (Exception ex) {
+			JOptionPane.showMessageDialog(null, ex.getMessage());
+			return;
+		}
+
+	}
+
+	public void getOrientation() {
+		String camera = core_.getCameraDevice();
+		if (camera == null) {
+			JOptionPane.showMessageDialog(null,
+					"This function does not work without a camera");
+			return;
+		}
+		try {
+			String tmp = core_.getProperty(camera, "TransposeCorrection");
+			if (tmp.equals("0"))
+				correction_ = false;
+			else
+				correction_ = true;
+			tmp = core_.getProperty(camera, MMCoreJ
+					.getG_Keyword_Transpose_MirrorX());
+			if (tmp.equals("0"))
+				mirrorX_ = false;
+			else
+				mirrorX_ = true;
+			tmp = core_.getProperty(camera, MMCoreJ
+					.getG_Keyword_Transpose_MirrorY());
+			if (tmp.equals("0"))
+				mirrorY_ = false;
+			else
+				mirrorY_ = true;
+			tmp = core_.getProperty(camera, MMCoreJ
+					.getG_Keyword_Transpose_SwapXY());
+			if (tmp.equals("0"))
+				transposeXY_ = false;
+			else
+				transposeXY_ = true;
+		} catch (Exception exc) {
+			exc.printStackTrace();
+			JOptionPane
+					.showMessageDialog(null,
+							"Exception encountered. Please report to the Micro-Manager support team");
+			return;
+		}
+	}
 
 }
