59e426cd7804b3904675c659b7a834823d5495ab
nico <nico@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2008-12-24 06:09:55 +0000

    Debian build from Johan
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/branches/micromanager1.3@1891 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- mkdebian.sh
+++ mkdebian.sh
@@ -1,55 +1,62 @@
-ROOT=../root
-rm -Rf $ROOT
-mkdir $ROOT
+#portable files + binary (almost none portable)
+ROOTPORT=../rootport
+rm -Rf $ROOTPORT
+mkdir $ROOTPORT
+
+ROOTBIN=$ROOTPORT
+
+#micromanager-imagej
+ROOTIJ=../rootij
+rm -Rf $ROOTIJ
+mkdir $ROOTIJ
 
 ##### Meta information
-mkdir $ROOT/DEBIAN
-cp doc/copyright.txt $ROOT/DEBIAN/copyright
-cp debiancontrol $ROOT/DEBIAN/control
-echo "#!/bin/sh" > $ROOT/DEBIAN/postinst
-chmod 0555 $ROOT/DEBIAN/postinst
+mkdir $ROOTPORT/DEBIAN
+mkdir $ROOTIJ/DEBIAN
+cp doc/copyright.txt $ROOTPORT/DEBIAN/copyright
+cp doc/copyright.txt $ROOTIJ/DEBIAN/copyright
+cp debiancontrol.port $ROOTPORT/DEBIAN/control
+cp debiancontrol.ij $ROOTIJ/DEBIAN/control
+echo "#!/bin/sh" > $ROOTBIN/DEBIAN/postinst
+chmod 0555 $ROOTBIN/DEBIAN/postinst
 
 ##### Programs
-mkdir -p $ROOT/usr/bin/
-cp Test_Serial/mm_testserial ModuleTest/mm_moduletest Test_MMCore/mm_testCore $ROOT/usr/bin/
-strip $ROOT/usr/bin/*
-cp runMMstudioDebian.sh $ROOT/usr/bin/mmstudio
+mkdir -p $ROOTBIN/usr/bin/
+cp Test_Serial/mm_testserial ModuleTest/mm_moduletest Test_MMCore/mm_testCore $ROOTBIN/usr/bin/
+strip $ROOTBIN/usr/bin/*
 
 ##### JAR-files
-mkdir -p $ROOT/usr/share/imagej/plugins/Micro-Manager/
+mkdir -p $ROOTIJ/usr/share/imagej/plugins/Micro-Manager/
 cp Bleach/MMBleach_.jar autofocus/MMAutofocus_.jar autofocus/MMAutofocusTB_.jar MMCoreJ_wrap/MMCoreJ.jar Tracking/Tracker_.jar \
-mmstudio/MMJ_.jar mmstudio/MMReader_.jar $ROOT/usr/share/imagej/plugins/Micro-Manager/
+mmstudio/MMJ_.jar $ROOTIJ/usr/share/imagej/plugins/Micro-Manager/
 
-mkdir -p $ROOT/usr/share/java/
-cp MMCoreJ_wrap/MMCoreJ.jar $ROOT/usr/share/java/
+mkdir -p $ROOTPORT/usr/share/java/
+cp MMCoreJ_wrap/MMCoreJ.jar $ROOTPORT/usr/share/java/
 	#####classext/bsh-2.0b4.jar classext/syntax.jar classext/ij.jar #assume these are available
 
 ##### Core
-mkdir -p $ROOT/usr/lib/micro-manager/
+mkdir -p $ROOTBIN/usr/lib/micro-manager/
 
-cp MMCoreJ_wrap/.libs/libMMCoreJ_wrap.so $ROOT/usr/lib/micro-manager/
+cp MMCoreJ_wrap/.libs/libMMCoreJ_wrap.so $ROOTBIN/usr/lib/micro-manager/
 
 ##### All plugins
 for from in `ls DeviceAdapters/*/.libs/*.so`
 	do
 		to=${from##*/}
-		to=$ROOT/usr/lib/micro-manager/libmmgr_dal_`echo $to|sed 's/\..\{2\}$//'`
+		to=$ROOTBIN/usr/lib/micro-manager/libmmgr_dal_`echo $to|sed 's/\..\{2\}$//'`
 		to=$to.so
 		cp $from $to
 	done
-strip $ROOT/usr/lib/micro-manager/*
+strip $ROOTBIN/usr/lib/micro-manager/*
 
 ##### Make shared objects visible without hardcoding the path
-mkdir -p $ROOT/etc/ld.so.conf.d/
-echo "/usr/lib/micro-manager" > $ROOT/etc/ld.so.conf.d/micro-manager.conf
-echo "/sbin/ldconfig" >> $ROOT/DEBIAN/postinst
+mkdir -p $ROOTPORT/etc/ld.so.conf.d/
+echo "/usr/lib/micro-manager" > $ROOTPORT/etc/ld.so.conf.d/micro-manager.conf
+echo "/sbin/ldconfig" >> $ROOTBIN/DEBIAN/postinst
 
 ##### Put together
 cd ..
-dpkg-deb -b root
-mv root.deb micro-manager.deb
-
-#use later. buggy now
-#jpackage-utils
-
-
+dpkg-deb -b rootport
+dpkg-deb -b rootij
+mv rootport.deb micromanager.deb
+mv rootij.deb micromanager-ij.deb
