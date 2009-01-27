8d90c6dc3a99fe5aa8e690d6b4516a7a118686c0
OD <OD@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2009-01-27 16:25:45 +0000

    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/branches/micromanager1.3@2029 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- mmstudio/src/org/micromanager/utils/MMLogger.java
+++ mmstudio/src/org/micromanager/utils/MMLogger.java
@@ -1,18 +1,34 @@
 package org.micromanager.utils;
 
+import java.io.IOException;
+import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 
 public class MMLogger {
-   private static Logger logger_;
-   public MMLogger() {
-      if (logger_ == null)
-         logger_ = Logger.getLogger(this.getClass().getName());
-   }
-   
-   public static Logger getLogger() {
-      if (logger_ == null)
-         new MMLogger();
-      return logger_;
-   }
+	private static Logger logger_;
+
+	public MMLogger()
+	// !!! ToDo: specify exceptions throws SecurityException, IOException {
+	{
+		try {
+			if (logger_ == null) {
+				logger_ = Logger.getLogger(this.getClass().getName());
+				logger_.addHandler(new FileHandler("MMStudio.log"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static Logger getLogger() {
+		// !!! ToDo: specify exceptions throws SecurityException, IOException {
+		try {
+			if (logger_ == null)
+				new MMLogger();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return logger_;
+	}
 
 }
