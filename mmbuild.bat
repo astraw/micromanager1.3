f2de2fcc4ba084f9bc08bbd13df395f75440d158
OD <OD@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2009-02-14 00:57:28 +0000

    Add Tracked
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/branches/micromanager1.3@2110 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- mmbuild.bat
+++ mmbuild.bat
@@ -11,6 +11,11 @@ cd autofocus
 call \projects\3rdparty\apache-ant-1.6.5\bin\ant -buildfile build.xml cleanAutofocus compileAutofocus buildAutofocus 
 cd ..
 
+
+cd plugins\Tracker 
+call \projects\3rdparty\apache-ant-1.6.5\bin\ant -buildfile build.xml cleanMMTracking compileMMTracking buildMMTracking 
+cd ..
+
 cd mmStudio\src
 call \projects\3rdparty\apache-ant-1.6.5\bin\ant -buildfile ..\build.xml cleanMMStudio compileMMStudio buildMMStudio buildMMReader install packInstaller
 
