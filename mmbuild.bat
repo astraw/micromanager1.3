870b8547f04e6ec0986e182934b9f7fa31796459
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2009-03-27 20:03:43 +0000

    Updated Windows builds scripts.
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/branches/micromanager1.3@2291 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- mmbuild.bat
+++ mmbuild.bat
@@ -3,6 +3,7 @@ cd "\Program Files\Microsoft Visual Studio 9.0\VC\"
 call vcvarsall.bat
  
 cd "\projects\micromanager1.3\"
+devenv /REBUILD Debug .\MMCore\MMCore.vcproj
 devenv /REBUILD Release .\MMCoreJ_wrap\MMCoreJ_wrap.sln
 
 ECHO Building Java components...
@@ -17,7 +18,7 @@ call \projects\3rdparty\apache-ant-1.6.5\bin\ant -buildfile build.xml cleanMMTra
 cd ..\..
 
 cd mmStudio\src
-call \projects\3rdparty\apache-ant-1.6.5\bin\ant -buildfile ..\build.xml cleanMMStudio compileMMStudio buildMMStudio buildMMReader install packInstaller
+call \projects\3rdparty\apache-ant-1.6.5\bin\ant -buildfile ..\build.xml cleanMMStudio compileMMStudio buildMMStudio buildMMReader install makeDeviceList packInstaller
 
 ECHO "Done"
 cd "..\.."
