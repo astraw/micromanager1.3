dcac64f02218df18945c11790df350fb7118f6b5
arthur <arthur@d0ab736e-dc22-4aeb-8dc9-08def0aa14fd>
2009-04-09 16:25:09 +0000

    New version
    git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/branches/micromanager1.3@2336 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

--- mmstudio/src/org/micromanager/MMStudioMainFrame.java
+++ mmstudio/src/org/micromanager/MMStudioMainFrame.java
@@ -148,7 +148,7 @@ public class MMStudioMainFrame extends JFrame implements DeviceControlGUI,
 		ScriptInterface {
 
 	private static final String MICRO_MANAGER_TITLE = "Micro-Manager 1.3";
-	private static final String VERSION = "1.3.36";
+	private static final String VERSION = "1.3.37";
 	private static final long serialVersionUID = 3556500289598574541L;
 
 	private static final String MAIN_FRAME_X = "x";
